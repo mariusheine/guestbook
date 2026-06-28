7bcd0fee5f0abb79b0611c08f796abaa3d7e104d
Marius Heine <m.heine@geprog.com>
2026-06-29 00:49:47 +0200

    print in light mode

--- app.vue
+++ app.vue
@@ -17,4 +17,18 @@ onErrorCaptured((error) => {
     color: 'error',
   });
 });
+
+let hadDarkClass = false;
+
+onMounted(() => {
+  window.addEventListener('beforeprint', () => {
+    hadDarkClass = document.documentElement.classList.contains('dark');
+    document.documentElement.classList.remove('dark');
+  });
+  window.addEventListener('afterprint', () => {
+    if (hadDarkClass) {
+      document.documentElement.classList.add('dark');
+    }
+  });
+});
 </script>
