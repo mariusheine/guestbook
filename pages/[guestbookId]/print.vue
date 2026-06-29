8bc8f0d8d90c34df5cc9d63424efa32697c49850
Marius Heine <m.heine@geprog.com>
2026-06-29 14:18:28 +0200

    central component for guestbook entry

--- pages/[guestbookId]/print.vue
+++ pages/[guestbookId]/print.vue
@@ -14,25 +14,7 @@
       :key="page.createdAt"
       class="w-full print:h-screen print:flex print:flex-col print:items-center print:justify-center print:not-last:break-after-page print:break-inside-avoid"
     >
-      <UCard class="w-full print:max-w-2xl">
-        <template #header>
-          <div class="flex justify-between items-center">
-            <div>
-              Von
-              <UBadge color="primary" class="text-xl">
-                {{ page.author || "Anonymous" }}
-              </UBadge>
-            </div>
-            <div>{{ formatTimestamp(page.createdAt) }}</div>
-          </div>
-        </template>
-        <div class="space-y-4 text-center">
-          <div class="whitespace-pre-line">
-            {{ page.content }}
-          </div>
-          <img :src="page.image" class="mx-auto">
-        </div>
-      </UCard>
+      <GuestbookEntry :page="page" hide-image-context class="w-full print:max-w-2xl" />
     </div>
   </template>
 </template>
