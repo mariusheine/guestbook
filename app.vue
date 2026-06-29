54c8a53d6e003d23429f3b2eca843723f9436986
Marius Heine <m.heine@geprog.com>
2026-06-29 20:44:38 +0200

    Pdf export (#8)
    * initial draft of printing
    
    * print in light mode
    
    * fix leading and trailing blank page
    
    * avoid duplicate clicking on print button
    
    * add title page in pdf
    
    * preserve linebreaks in page content
    
    * central component for guestbook entry

--- app.vue
+++ app.vue
@@ -17,4 +17,23 @@ onErrorCaptured((error) => {
     color: 'error',
   });
 });
+
+const route = useRoute();
+
+let hadDarkClass = false;
+
+onMounted(() => {
+  window.addEventListener('beforeprint', () => {
+    hadDarkClass = document.documentElement.classList.contains('dark');
+    document.documentElement.classList.remove('dark');
+  });
+  window.addEventListener('afterprint', () => {
+    if (hadDarkClass) {
+      document.documentElement.classList.add('dark');
+    }
+    if (route.path.endsWith('/print')) {
+      navigateTo(route.path.replace(/\/print$/, ''));
+    }
+  });
+});
 </script>
